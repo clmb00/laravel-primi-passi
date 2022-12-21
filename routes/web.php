10338cfc12ff00a79cfee7235c7518e9ac8f6be5
clmb00 <paolo.colombo2000@gmail.com>
2022-12-21 16:32:54 +0100

    [add] homepage

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+
+    $msg = 'Hello';
+    $user = 'john';
+
+    return view('home', compact('msg','user'));
 });
